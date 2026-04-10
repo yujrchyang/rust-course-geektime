92d6a30be60ceb98d68a1195a9b45ce1debcab94
yujrchyang <yujrchyang@gmail.com>
2026-04-10 10:31:04 +0800

    feat: add thumbor example

---
+++ 05_thumbor/src/engine/mod.rs
@@ -0,0 +1,17 @@
+use crate::pb::Spec;
+use image::ImageOutputFormat;
+
+mod photon;
+pub use photon::Photon;
+
+pub trait Engine {
+    // 对 engine 按照 specs 进行一系列有序的处理
+    fn apply(&mut self, specs: &[Spec]);
+    // 从 engine 中生成目标图片
+    fn generate(self, format: ImageOutputFormat) -> Vec<u8>;
+}
+
+pub trait SpecTransform<T> {
+    // 对图片使用 op 做 transform
+    fn transform(&mut self, op: T);
+}
